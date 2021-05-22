a426db3c21ab3001bd69970872bb97c32ad2313b
abarrerah <alum.abarrerah@iesalixar.org>
2021-05-22 16:04:55 +0200

    Keep working on Stories

--- src/learnatorium-front/src/pages/Stories.tsx
+++ src/learnatorium-front/src/pages/Stories.tsx
@@ -1,40 +1,36 @@
 /* eslint-disable array-callback-return */
-/* eslint-disable jsx-a11y/heading-has-content */
-/* eslint-disable react-hooks/exhaustive-deps */
+import React, { Component } from "react";
 
-/* eslint-disable react-hooks/rules-of-hooks */
-import axios from "axios";
-import React, { useState } from "react";
-
-function Stories() {
-
-  const [story,setStory] =useState<[]>([]);
-
-  const apiURL= "http://localhost:8050/documents";
-
-  const fetchData = async()=>{
-    for(let i = 0; i < 3;i++){
-      const response = await axios.get(apiURL);
-      setStory(JSON.parse(JSON.stringify(response.data)));
-    }
-
-  
-    for(let value of story){
-      // @ts-ignore
-      console.log(value.name);
-    }
-   
+export default class Stories extends Component {
+  constructor(props: any) {
+    super(props);
+    this.state = {};
   }
-   
-  
-  return (
-      <div >
-          <button onClick={fetchData}>del</button>
-          <div >
-            
-          </div>
-      </div>
-  );
-}
 
-export default Stories;
+  async componentDidMount() {
+    await fetch("http://localhost:8050/documents")
+      .then((response) =>
+        response.json().then((result) => {
+          this.setState(result);
+        })
+      )
+      .catch((err) => console.log(err));
+  }
+
+  render() {
+    const result23 = JSON.parse(JSON.stringify(this.state));
+
+    return (
+      <div className="uk-child-width-1-2@s uk-grid-match" uk-grid>
+        {Object.keys(result23).map((element) => {
+          return (
+            <div className="uk-card uk-card-hover uk-card-body">
+              <div className="uk-card-title">{result23[element].name}</div>
+              <p>{result23[element].content}</p>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
